42b0fdf02325df2ee22ad30ebad98cb05437a40f
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-11-09 13:28:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 //your code here
 let box = document.getElementById("box");
-box.innerText = "This is a demo of box model, try to replicate this box model in Html & Css and pass all the test cases."
+box.innerText = "This is a demo of box model try to replicate this box model in Html and Css and try to pass all the test cases."
